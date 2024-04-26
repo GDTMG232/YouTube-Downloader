3cef6a9a10d4c4f900c291b90f09bb03408bb20e
GDTMG232 <157226295+GDTMG232@users.noreply.github.com>
2024-04-26 18:15:59 +0100

    Ver Check
    Added version checker

--- YouTube Downloader.py
+++ YouTube Downloader.py
@@ -1,10 +1,40 @@
-from fflocation import fflocation
-
 try:
    import yt_dlp as youtube_dl
+   import requests
 except:
-   input("You do not have yt_dlp, please run InstallYTDLP.bat to install it.\nPress enter to exit.")
-   exit()
+   print("You do not have yt_dlp, please run InstallYTDLP.bat to install it.")
+
+version = "V0.01"
+
+def read_github_file(raw_url):
+    try:
+        response = requests.get(raw_url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching file: {e}")
+        return None
+
+def download_file(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print("File downloaded successfully!")
+    else:
+        print("Failed to download file. Status code:", response.status_code)
+
+currentVersion = ""
+changelog = ""
+
+exec(read_github_file("https://raw.githubusercontent.com/GDTMG232/YouTube-Downloader/main/updates.py"))
+if currentVersion != version:
+   if input(f"You have to update to {currentVersion}!\n{changelog}\n\nWould you like to update?\nY/N: ").lower() == "y":
+      download_file("https://raw.githubusercontent.com/GDTMG232/YouTube-Downloader/main/YouTube%20Downloader.py", "NewVersionYTDNLDR.py")
+      input("Downloaded! Run NewVersionYTDNLDR.py for the newest version!\nPress Enter to quit.")
+      exit()
+   else:
+      print("\nOkay!\n")
 
 def download_audio(url, output_path = ""):
     options = {
@@ -14,7 +44,6 @@ def download_audio(url, output_path = ""):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'ffmpeg_location': fflocation,
         'outtmpl': output_path + '/%(title)s',
     }
     
@@ -25,13 +54,13 @@ def download_audio(url, output_path = ""):
 def download_video(url, output_path = ""):
     ydl_opts = {
         'format': 'best',
-        'ffmpeg_location': fflocation,
         'outtmpl': output_path + '/%(title)s' + ".mp4",
     }
     
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
     print("Video downloaded successfully.")
+    
 try:
     while True:
       try:
@@ -48,7 +77,7 @@ try:
            print("ok bye")
            exit()
         else:
-          print("Try again.\n")
+          print("wrong")
       except Exception as e:
         print(e)
         pass
