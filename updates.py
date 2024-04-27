4ebeac22de52ee57edcb6435a9f7c69e411076ea
GDTMG232 <157226295+GDTMG232@users.noreply.github.com>
2024-04-27 12:11:20 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,3 @@
-currentVersion = "V0.1"
-changelog = "Added Updater"
+currentVersion = "V0.11"
+changelog = "Added reminder for 'pip install --upgrade yt_dlp'"
 # This Python file is simply just versions, no worries about downloading this file :)
