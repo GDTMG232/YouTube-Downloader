70dde8aced9f63c019969c2b7286d6b4caeda483
GDTMG232 <157226295+GDTMG232@users.noreply.github.com>
2024-05-26 21:58:04 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,3 @@
-currentVersion = "V0.12"
-changelog = "Added reminder for 'pip install --upgrade yt_dlp' -- renamed an to av"
+currentVersion = "V0.13"
+changelog = "Added function for CMD - allows perameters"
 # This Python file is simply just versions, no worries about downloading this file :)
