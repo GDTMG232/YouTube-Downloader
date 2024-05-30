2443b6fc782043f43e43e0f6c060be086f44a20b
TMG <157226295+GDTMG232@users.noreply.github.com>
2024-05-30 13:07:54 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,3 @@
-currentVersion = "V0.13"
-changelog = "Added function for CMD - allows perameters"
+currentVersion = "V0.135"
+changelog = "Added function for CMD - allows perameters -- Forgot fflocation.txt | Fixed now"
 # This Python file is simply just versions, no worries about downloading this file :)
