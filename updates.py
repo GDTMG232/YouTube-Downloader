dcc52d913fdd5cec40787e4401457c27f9493d12
TMG <157226295+GDTMG232@users.noreply.github.com>
2024-06-16 17:40:34 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,3 @@
-currentVersion = "V0.14"
-changelog = "fflocation.txt will be found whether you are using YouTube Downloader.py as a command script or not"
+currentVersion = "V0.15"
+changelog = "Entire script rewrite"
 # This Python file is simply just versions, no worries about downloading this file :)
