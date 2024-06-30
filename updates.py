3d8e7abdbec835470ad82561affc665a81ce0bc1
TMG <157226295+GDTMG232@users.noreply.github.com>
2024-06-30 11:17:37 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,3 @@
-currentVersion = "V0.15"
-changelog = "Entire script rewrite"
+currentVersion = "V0.16"
+changelog = "Always best quality download"
 # This Python file is simply just versions, no worries about downloading this file :)
