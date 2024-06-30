ea1271fcd1af7016a74cad3019a4f0d9e0a71f6d
TMG <157226295+GDTMG232@users.noreply.github.com>
2024-06-30 11:25:02 +0100

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,3 +1,35 @@
 currentVersion = "V0.16"
 changelog = "Always best quality download"
 # This Python file is simply just versions, no worries about downloading this file :)
+'''
+ALL CHANGES - in order
+
+V0.01
+Added Versions and Updates.
+
+V0.1
+Added Updater
+
+V0.11
+Added reminder for 'pip install --upgrade yt_dlp'
+
+V0.12
+Minor Fix : renamed -- an (audio & video) to av
+
+V0.13
+Added function for CMD - allows perameters
+ - You can run the downloader with CMD
+	"Youtube Downloader.py" v "YOUR-URL"
+
+V0.135
+Minor Fix : Forgot to add fflocation.txt to the YoutubeDownloader file
+
+V0.14
+fflocation.txt will the found regardless of interactive mode or command mode
+
+V0.15
+Entire rewrite - same functions as before however
+
+V0.16
+Always download in best quality
+```
